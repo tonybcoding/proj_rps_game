f26f2aaf29f8d033d839380182abaf117dafd68a
Tony Burge <tonymburge@hotmail.com>
2020-07-03 15:41:28 -0400

    v0.13 just nailed getting player object and opponent objects passed

--- rps-burge.py
+++ rps-burge.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 legal_moves = ["rock", "paper", "scissors"]
@@ -13,30 +14,53 @@ class Game:
 
     def __init__(self):
         #
-        # request user name and store it
-        # request game mode, set game type and get number of rounds
-        # determine computer v computer or human v computer
+        # display intro, request user's name and 
+        # initialize game settings
         self.__display_intro()
         self.__game_settings()
         #
-        # create player objects
+        # for each gamer in list, instantiate the appropriate player object
+        self.player_list = []
+        for gamer in self.game_player_list:
+            self.player_list.append(self.__instantiate_player(gamer[1]))
         #
         # play game
-        #
-        # show results
+        self.__play_game()
 
+    def __play_game(self):
+        print(f"\n{self.player_list[0].name} and {self.player_list[1].name} "
+              f"have entered the ring!")
+        print(f"Game starts now!\n")
+        # sending opponent object so current play can compare
+        self.player_list[0].throw(self.player_list[1])
+        self.player_list[1].throw(self.player_list[0])
+
+
+    def __instantiate_player(self, player):
+        #
+        if player[0] == 0:
+            new_player = HumanPlayer(player)
+        elif player[0] == 1:
+            new_player = RandomPlayer(player)
+        elif player[0] == 2:
+            new_player = ReflectPlayer(player)
+        elif player[0] == 3:
+            new_player = CyclePlayer(player)
+        else: # must be last option
+            new_player = SmartPlayer(player)
+        #
+        return new_player
 
     def __game_settings(self):
         self.user = self.__request_name()
         self.game_type = self.__set_game_type()
         self.game_rounds = self.__num_of_rounds()
-        self.game_player_types = self.__set_player_types()      
+        self.game_player_list = self.__set_player_types()
         print(f"\n\nOkay, we're all set, {self.user}!")
         print(f"Let's play {self.game_rounds} rounds of {self.game_type[3]}: "
-            f"{self.game_player_types[0][1]} VS. "
-            f"{self.game_player_types[1][1]}")
+              f"{self.game_player_list[0][1]} VS. "
+              f"{self.game_player_list[1][1]}")
         print(f"")
-
 
     def __set_player_types(self):
         #
@@ -56,15 +80,14 @@ class Game:
         #
         return player_type_list
 
-
     def __computer_player_type(self, player_num):
         #
         type = []
         #
         # print menu of options, get input, and append to list
-        options = [ [1, "PC-Random"], [2, "PC-Reflects"], [3, "PC-Cycles"],
-                    [4, "PC-Learns"]]
-        print(f"\nPlease select computer player {player_num} type:")
+        options = [[1, "PC-Random"], [2, "PC-Reflects"], [3, "PC-Cycles"],
+                   [4, "PC-Learns"]]
+        print(f"\nPlease select computer player ## {player_num} ## type:")
         self.__print_options(options)
         print("(5) Surprise me!")
         type.append(self.__get_int_value(1, 5))
@@ -79,16 +102,14 @@ class Game:
                 type.append(n[1])
         return type
 
-
     def __print_options(self, menu_list):
         #
         # menu_list in form of [[item 1, desc], [item 2, desc], etc.]
         for n in menu_list:
             print(f"({n[0]}) {n[1]}")
 
-
     def __get_int_value(self, low, high):
-        # 
+        #
         while True:
             try:
                 value = int(input(">>> "))
@@ -96,14 +117,13 @@ class Game:
                     break
                 else:
                     print(f"Please enter a value between {low} and {high}.")
-            except:
+            except ValueError:
                 print("Please enter a number.")
         return value
 
-
     def __is_human_playing(self):
         #
-        options = [[1, "Play against the Computer"], 
+        options = [[1, "Play against the Computer"],
                    [2, "Have Computer play against a second Computer player"]]
         print("\nWhould you like to:")
         self.__print_options(options)
@@ -112,7 +132,6 @@ class Game:
             return True
         else:
             return False
-
 
     def __set_game_type(self):
         #
@@ -130,15 +149,13 @@ class Game:
             game_type_list = [2, 3, 100, "Best of Rounds"]
         return game_type_list
 
-
     def __request_game_mode(self):
         #
-        options = [ [1, "Set Rounds"], [2, "Best of Rounds"] ]
+        options = [[1, "Set Rounds"], [2, "Best of Rounds"]]
         print(f"\nWould you like to play:")
         self.__print_options(options)
         mode = self.__get_int_value(1, 2)
         return mode
-
 
     def __num_of_rounds(self):
         #
@@ -147,7 +164,6 @@ class Game:
         print(f"\nPlease enter number of rounds ({min} to {max}):")
         rounds = self.__get_int_value(min, max)
         return rounds
-
 
     def __request_name(self):
         #
@@ -159,7 +175,6 @@ class Game:
             else:
                 break
         return user_name
-
 
     def __display_intro(self):
         #
@@ -175,7 +190,7 @@ class Game:
             print("/" * banner_len)
         print("\n")
         print("Please allow me to ask a few questions to "
-            "set up your game...\n")
+              "set up your game...\n")
 
 
 ###############################################################################
@@ -186,30 +201,24 @@ class Game:
 class Player:
     #
     # all subclasses to execute setting of name
-    def __init__(self, entered_name):
-        self.name = self.setName(entered_name)
-    #
-    def setName(self, entered_name):
-        #
-        # if entered_name is blank,
-        # create a name from 4 to 12 characters long using random
-        # ASCII characters
-        name = ""
-        if entered_name == "":
-            for n in range(random.randint(4, 12)):
-                name += chr(random.randint(97, 122))
-        else:
-            name = entered_name
-        return name.capitalize()
+    def __init__(self, player_name):
+        self.name = player_name
+        self.move_list = []
+    
+    def throw(self, opponent):
+        print(f"{self.name} is throwing against {opponent.name}")
+
+    def __learn(self, opponent):
+        pass
+
+
 
 
 class RandomPlayer(Player):
     pass
 
-
-class RefelctPlayer(Player):
+class ReflectPlayer(Player):
     pass
-
 
 class CyclePlayer(Player):
     pass
@@ -221,12 +230,14 @@ class SmartPlayer(Player):
 
 class HumanPlayer(Player):
     pass
-
-
+    # def __init__(self, player_type):
+    #     super().__init__(player_type) # ensure super class init code is executed
+    #     print("You're a human!")
 
 ###############################################################################
 ###############################################################################
 ###############################################################################
+
 
 # main
 if __name__ == '__main__':
